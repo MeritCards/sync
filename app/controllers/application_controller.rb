0e9c89ff350836e794b3211e140cbcaa73c93fa6
Eroica <Eroica@noreply.users.github.com>
2024-12-05 15:59:42 +0100

    Ignore layout in "API requests"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  include ApplicationHelper
   include SessionsHelper
+
+  # Disable full HTML layout for API requests
+  # API request = Something that authenticates via basic auth
+  layout -> { basic_auth_request? ? false : "application" }
 end
