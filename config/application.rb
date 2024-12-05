2353de8a438d700ecea5b54264a2323ade32475e
Eroica <Eroica@noreply.users.github.com>
2024-12-05 16:22:17 +0100

    Remove ActiveStorage routes

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,7 @@ module Sync
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.active_storage.draw_routes = false
   end
 end
